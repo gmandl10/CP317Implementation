9e04e2d0b0831ecaf532cac9929cbb20f975cefe
George Mandl <79493081+gmandl10@users.noreply.github.com>
2022-04-05 12:39:00 -0400

    Update LeagueController.java

--- demo/src/main/java/com/LeagueController.java
+++ demo/src/main/java/com/LeagueController.java
@@ -1,4 +1,4 @@
-package com.javatpoint.controller;
+package com.controller;
 
 import java.util.List;
 
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javatpoint.model.League;
-import com.javatpoint.service.LeagueService;
+import com.model.League;
+import com.service.LeagueService;
 
 @RestController
 public class LeagueController {
