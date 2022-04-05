4f8317c182d0e410997303ca31678eeb9a382227
George Mandl <79493081+gmandl10@users.noreply.github.com>
2022-04-05 12:42:50 -0400

    Update UserService.java

--- demo/src/main/java/com/UserService.java
+++ demo/src/main/java/com/UserService.java
@@ -1,4 +1,4 @@
-package com.javatpoint.service;
+package com.service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,8 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.javatpoint.model.User;
-import com.javatpoint.repository.UserRepository;
+import com.model.User;
+import com.repository.UserRepository;
 
 @Service
 public class UserService {
