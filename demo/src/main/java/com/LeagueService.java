2d72d8d46cd626c23f7a76c915acc25053362212
George Mandl <79493081+gmandl10@users.noreply.github.com>
2022-04-05 12:39:35 -0400

    Update LeagueService.java

--- demo/src/main/java/com/LeagueService.java
+++ demo/src/main/java/com/LeagueService.java
@@ -1,4 +1,4 @@
-package com.javatpoint.service;
+package com.service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,8 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.javatpoint.model.League;
-import com.javatpoint.repository.LeagueRepository;
+import com.model.League;
+import com.repository.LeagueRepository;
 
 @Service
 public class LeagueService {
