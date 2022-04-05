189f31e8d42c7c9cea6c4af55fcc9b0150163eac
George Mandl <79493081+gmandl10@users.noreply.github.com>
2022-04-05 12:41:42 -0400

    Update TeamService.java

--- demo/src/main/java/com/TeamService.java
+++ demo/src/main/java/com/TeamService.java
@@ -1,4 +1,4 @@
-package com.javatpoint.service;
+package com.service;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,8 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.javatpoint.model.Team;
-import com.javatpoint.repository.TeamRepository;
+import com.model.Team;
+import com.repository.TeamRepository;
 
 @Service
 public class TeamService {
