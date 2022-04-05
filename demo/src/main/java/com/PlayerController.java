19aa4dbfe90326432dac45471c3f461623cfbb9c
George Mandl <79493081+gmandl10@users.noreply.github.com>
2022-04-05 16:12:22 -0400

    Update PlayerController.java

--- demo/src/main/java/com/PlayerController.java
+++ demo/src/main/java/com/PlayerController.java
@@ -1,4 +1,4 @@
-package com.controller;
+package com;
 
 import java.util.List;
 
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.model.Player;
-import com.service.PlayerService;
+import com.Player;
+import com.PlayerService;
 
 
 
