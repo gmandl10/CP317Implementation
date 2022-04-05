00df4fb31847f67ff98c9170894a4f7bf3989a57
George Mandl <79493081+gmandl10@users.noreply.github.com>
2022-04-05 16:07:39 -0400

    Update GameController.java

--- demo/src/main/java/com/GameController.java
+++ demo/src/main/java/com/GameController.java
@@ -1,4 +1,4 @@
-package com.controller;
+package com;
 
 import java.util.List;
 
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.model.Game;
-import com.service.GameService;
+import com.Game;
+import com.GameService;
 
 @RestController
 public class GameController {
