4c554011add63d90e97cf697f0b50c4b2124c4f0
George Mandl <79493081+gmandl10@users.noreply.github.com>
2022-04-05 16:08:52 -0400

    Update GameService.java

--- demo/src/main/java/com/GameService.java
+++ demo/src/main/java/com/GameService.java
@@ -1,4 +1,4 @@
-package com.service;
+package com;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,8 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.model.Game;
-import com.repository.GameRepository;
+import com.Game;
+import com.GameRepository;
 
 @Service
 public class GameService {
