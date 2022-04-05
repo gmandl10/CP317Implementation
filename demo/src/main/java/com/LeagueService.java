880ba2eda72de194daffb944c353539dfcb42eff
Hamzah Raza <Hamzah.raza99@gmail.com>
2022-04-05 16:18:01 -0400

    updating links

--- demo/src/main/java/com/LeagueService.java
+++ demo/src/main/java/com/LeagueService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.model.League;
+import com.League;
 import com.repository.LeagueRepository;
 
 @Service
