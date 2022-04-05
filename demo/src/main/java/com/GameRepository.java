c0bf98b0982e2bd86478446dc72b9a3399fe855c
George Mandl <79493081+gmandl10@users.noreply.github.com>
2022-04-05 12:36:19 -0400

    Update GameRepository.java

--- demo/src/main/java/com/GameRepository.java
+++ demo/src/main/java/com/GameRepository.java
@@ -1,6 +1,6 @@
 package com.javatpoint.repository;
 import org.springframework.data.repository.CrudRepository;
-import com.javatpoint.model.Game;
+import com.model.Game;
 public interface GameRepository extends CrudRepository<Game,Integer>{
 
 }
